812db82637bdc0408771fb957cdfb614b1557b76
Biswesh-Kumar-Padhy <163874726+Biswesh-Kumar-Padhy@users.noreply.github.com>
2025-09-17 09:50:54 +0530

    Time: 3 ms (75.25%) | Memory: 51 MB (9.39%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,22 +1,39 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int n= nums.length;
-        for(int i=0; i<n; i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i],map.get(nums[i])+1);
+        int count1 = 0; int count2 = 0;
+        int max1 = -1; int max2 = -1;
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i<nums.length; i++){
+            if(count1==0 && nums[i]!=max2){
+                count1 = 1;
+                max1 = nums[i];
             }
+            else if(count2==0 && nums[i]!=max1){
+                count2 = 1;
+                max2 = nums[i];
+            }
+            else if(nums[i]==max1)  count1++;
+            else if(nums[i]==max2)  count2++;
             else{
-                map.put(nums[i],1);
+                count1--;
+                count2--;
             }
         }
-        List<Integer> result = new ArrayList<>();
-        for(int key : map.keySet()){
-            if(map.get(key) > (n/3)){
-                result.add(key);
-            }
+
+    // Verify the max1, max2 is more than n/3
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for (int num : nums) {
+            if (num == max1)    cnt1++;
+            if (num == max2)    cnt2++;
         }
-        return result;
-        
+        if (cnt1 > nums.length/3) {
+            list.add(max1);
+        }
+        if(cnt2 > nums.length/3 && max1!=max2) {
+            list.add(max2);
+        }
+        return list;
     }
 }
