90ce379dd9dd91ce7ed47264d2c6ba474227f5ac
Biswesh-Kumar-Padhy <163874726+Biswesh-Kumar-Padhy@users.noreply.github.com>
2025-11-04 20:48:07 +0530

    Time: 1 ms (99.96%) | Memory: 45.6 MB (52.77%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -1,17 +1,37 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int[] result = new int[2];
-        int idx = 0;
+        int n = nums.length - 2;
+        int XOR = 0;
 
-        for(int i = 0; i < nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        for (int num : nums){
+            XOR ^= num;
         }
-        for (int key : map.keySet()) {
-            if (map.get(key) == 2) {
-                result[idx++] = key;
+        for (int num = 0; num <= n-1; num++){
+            XOR ^= num;
+        }
+
+        // Find rightmost set bit
+        int trailZeroCount = Integer.numberOfTrailingZeros(XOR);
+        int mask = 1 << trailZeroCount;
+        int G1 = 0, G2 = 0;
+
+        // Separate into two groups based on mask bit
+        for(int num : nums){
+            if((num & mask) == 0){
+                G1 ^= num;
+            }
+            else{
+                G2 ^=num;
             }
         }
-        return result;        
+        for (int num = 0; num <= n-1; num++){
+            if((num & mask) == 0){
+                G1 ^= num;
+            }
+            else{
+                G2 ^=num;
+            }
+        }
+        return new int[]{G1, G2};
     }
 }
