63144a9963789bbb885af8cbcc5cb5bf8d69cf7e
Biswesh-Kumar-Padhy <163874726+Biswesh-Kumar-Padhy@users.noreply.github.com>
2025-11-06 16:00:16 +0530

    Time: 11 ms (24.34%) | Memory: 92.5 MB (46.95%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
@@ -1,19 +1,21 @@
 class Solution {
     public int minCost(String colors, int[] neededTime) {
         int n = colors.length();
-        int time = 0;
-        int prevMax = 0;
-        
-        for (int i = 0; i < n; i++) {           
-            if (i > 0 && colors.charAt(i) != colors.charAt(i - 1)) {
+        int prevMax = 0; // Stores the previous max time 
+        int time = 0; // Total time to remove balloons
+         
+        for (int i = 0; i < n; i++){
+            // If color changes, reset prevMax for new color group
+            if(i>0 && colors.charAt(i) != colors.charAt(i - 1)){
                 prevMax = 0;
             }
-
-            int curr = neededTime[i];            
-            time += Math.min(prevMax, curr);            
-            prevMax = Math.max(prevMax, curr);
+            int curr = neededTime[i];
+            // If curr balloon same as prev → remove the smaller one
+            time += Math.min(curr, prevMax);
+            // Keep the larger removal time balloon
+            prevMax = Math.max(curr, prevMax);
         }
-        
         return time;
+        
     }
 }
