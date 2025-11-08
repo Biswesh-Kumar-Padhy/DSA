08b6a5fe28fb939477dd4467f032f6aef54110c0
Biswesh-Kumar-Padhy <163874726+Biswesh-Kumar-Padhy@users.noreply.github.com>
2025-11-08 19:52:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (5.00%) - LeetSync

--- 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.java
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.java
@@ -1,13 +1,13 @@
 class Solution {
     public int minimumOneBitOperations(int n) {
-        int ans = 0;
+        int binary = 0;
         // Continue while n has set(1) bits
         while (n > 0) {
             // XOR accumulates the toggling pattern for current bit level
-            ans = ans ^ n;  // b3 =  g0 ^ g1 ^ g3 (gray code to binary)
+            binary = binary ^ n;  // b3 =  g0 ^ g1 ^ g3 (gray code to binary)
             n = n >> 1;    // Right shift to move to next higher bit
         }
 
-        return ans;     // Final XOR value gives minimum operations      
+        return binary;     // Final XOR value gives minimum operations      
     }
 }
