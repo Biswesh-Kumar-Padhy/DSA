4cd0f1b961d7e04d0ca804822a4492fb530e6a21
Biswesh-Kumar-Padhy <163874726+Biswesh-Kumar-Padhy@users.noreply.github.com>
2025-11-26 11:37:11 +0530

    Time: 45 ms (30.06%) | Memory: 73.9 MB (30.35%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.java
+++ 474-ones-and-zeroes/ones-and-zeroes.java
@@ -1,24 +1,45 @@
 class Solution {
+    private int[][][] dp;
+
+    public int solve(int[][] count, int m, int n, int idx){
+        if(idx >= count.length || (m == 0 && n == 0)){
+            return 0;
+        }
+
+        if (dp[m][n][idx] != -1){
+            return dp[m][n][idx];
+        }
+
+        int include = 0;
+        if(count[idx][0] <= m && count[idx][1] <= n){
+            include = 1 + solve(count, m-count[idx][0], n-count[idx][1], idx+1);
+        }
+        
+        int exclude = solve(count, m, n, idx + 1);
+        return dp[m][n][idx] = Math.max(include, exclude);
+    }
+
     public int findMaxForm(String[] strs, int m, int n) {
-        int[][] dp = new int[m + 1][n + 1];
+        int N = strs.length;
+        int[][] cnt = new int[N][2];
 
-        for (String str : strs) {
-            int zeros = 0, ones = 0;
-            for (char ch : str.toCharArray()) {
-                if (ch == '0')
-                    zeros++;
-                else
-                    ones++;
+        for (int i = 0; i < N; i++){
+            int zeroes = 0, ones = 0;
+            for(char ch : strs[i].toCharArray()){
+                if(ch == '1') ones++;
+                else zeroes++;
             }
+            cnt[i][0] = zeroes;
+            cnt[i][1] = ones;
+        }
 
-            // Traverse backward to avoid overwriting states we still need
-            for (int zero = m; zero >= zeros; zero--) {
-                for (int one = n; one >= ones; one--) {
-                    dp[zero][one] = Math.max(dp[zero][one], dp[zero - zeros][one - ones] + 1);
-                }
+        dp = new int[m + 1][n + 1][N];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                Arrays.fill(dp[i][j], -1);
             }
         }
 
-        return dp[m][n];
+        return solve(cnt, m, n, 0);
     }
 }
