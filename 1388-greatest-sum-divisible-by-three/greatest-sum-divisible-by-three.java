0c8f8916f71ac2fdebdca27a4e427e2f81db123f
Biswesh-Kumar-Padhy <163874726+Biswesh-Kumar-Padhy@users.noreply.github.com>
2025-11-29 15:06:08 +0530

    Time: 10 ms (50.82%) | Memory: 49.5 MB (56.38%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.java
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.java
@@ -1,26 +1,25 @@
 class Solution {
-    public int solve(int i, int rem, int[] nums, int[][] t){
-        if(i>= nums.length){
-            if(rem == 0){
-                return 0;
-            }
-            return Integer.MIN_VALUE;
-        }
-        if(t[i][rem] != -1){
-            return t[i][rem];
-        }
-
-        int take = nums[i] + solve(i+1, (rem + nums[i]) % 3, nums, t);
-        int skip = solve(i+1, rem, nums, t);
-
-        return t[i][rem] = Math.max(take, skip);
-    }
     public int maxSumDivThree(int[] nums) {
         int n = nums.length;
-        int[][] t = new int[n][3];
-        for(int[] row : t){
-            Arrays.fill(row, -1);
+        // t[i][rem] stores best sum from index i with remainder rem
+        int[][] t = new int[n+1][3];
+        for(int[] row : t){ // Initialize with int min
+            Arrays.fill(row, Integer.MIN_VALUE);
         }
-        return solve(0, 0, nums, t);
+        // Base case: i == n
+        t[n][0] = 0;
+        t[n][1] = Integer.MIN_VALUE;
+        t[n][2] = Integer.MIN_VALUE;
+
+        for(int i = n-1; i >= 0; i--){
+            for(int rem = 0; rem < 3; rem++){
+                int currRem = (rem + nums[i]) % 3;
+                int take = nums[i] + t[i+1][currRem];
+                int skip = t[i+1][rem];
+
+                t[i][rem] = Math.max(take, skip);
+            }
+        }
+        return t[0][0];
     }
 }
