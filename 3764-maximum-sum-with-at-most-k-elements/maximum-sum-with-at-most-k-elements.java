13ad84e283ccdb106aeac01019c9c0f75b8610b0
Biswesh-Kumar-Padhy <163874726+Biswesh-Kumar-Padhy@users.noreply.github.com>
2025-12-05 15:17:27 +0530

    Time: 209 ms (41.54%) | Memory: 143 MB (10.26%) - LeetSync

--- 3764-maximum-sum-with-at-most-k-elements/maximum-sum-with-at-most-k-elements.java
+++ 3764-maximum-sum-with-at-most-k-elements/maximum-sum-with-at-most-k-elements.java
@@ -1,22 +1,23 @@
 class Solution {
     public long maxSum(int[][] grid, int[] limits, int k) {
-        // List of pairs {value, rowIndex}
-        List<int[]> pairs = new ArrayList<>(); 
+        PriorityQueue<int[]> pairs = new PriorityQueue<>(
+            (a, b) -> b[0] - a[0]  // max-heap by value that sort descending 
+        );
+        
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[i].length; j++){
-                pairs.add(new int[]{grid[i][j], i}); // store value + row
+                pairs.offer(new int[]{grid[i][j], i}); // store value + row
             }
         }
-        pairs.sort((a, b) -> Integer.compare(b[0], a[0])); // sort descending 
 
         long maxSum = 0, len = 0;
         // Greedily pick the largest values while checking row limits
-        for(int[] p : pairs){
-            if(len == k)    break; // stop once we have taken k elements
+        while (len < k && !pairs.isEmpty()){
+            int[] top = pairs.poll();   // get max value
+            int value = top[0], row = top[1];
 
-            int key = p[0], row = p[1];
             if(limits[row] > 0){
-                maxSum += key; // add the value
+                maxSum += value; // add the value
                 limits[row]--; // reduce that row limit
                 len++; // increment total count
             }
