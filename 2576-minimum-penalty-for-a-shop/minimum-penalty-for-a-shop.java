ea08ecc9990d4eefd3a3d1165c32a8113f7d374e
Biswesh-Kumar-Padhy <163874726+Biswesh-Kumar-Padhy@users.noreply.github.com>
2025-12-26 20:23:19 +0530

    Time: 10 ms (80.69%) | Memory: 46.5 MB (85.52%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -2,36 +2,25 @@ class Solution {
     public int bestClosingTime(String customers) {
         int n = customers.length();
 
-        // prefixN[i] = number of 'N' customers in hours [0 .. i-1]
-        int[] prefixN = new int[n+1];
-        prefixN[0] = 0;
-        for(int i=1; i<=n; i++){
-            if(customers.charAt(i-1) == 'N') {
-                prefixN[i] = prefixN[i-1] + 1; //add penalty for open-while-N
-            } else {
-                prefixN[i] = prefixN[i-1];
-            }
+        // Initial penalty: count of 'Y' (closing at hour 0)
+        int penalty = 0;
+        for (char c : customers.toCharArray()) {
+            if (c == 'Y') penalty++;
         }
 
-        // suffixY[i] = number of 'Y' customers in hours [i .. n-1]
-        int[] suffixY = new int[n+1];
-        suffixY[n] = 0;
-        for(int i=n-1; i>=0; i--){
-            if(customers.charAt(i) == 'Y') {
-                suffixY[i] = suffixY[i+1] + 1; //add penalty for closed-while-Y
+        int minPenalty = penalty;
+        int minHour = 0;
+        for (int i = 0; i < n; i++) { // Try closing at every hour from 0 to n
+            if (customers.charAt(i) == 'Y') {
+                penalty--;   //desc penalty for closed-while-Y
             } else {
-                suffixY[i] = suffixY[i+1];
+                penalty++;   //inc penalty for open-while-N
             }
-        }
 
-        int minPenalty = Integer.MAX_VALUE;
-        int minHour    = 0; // best closing hour
-        for(int i=0; i<=n; i++){ // Try closing at every hour from 0 to n
-            // Total penalty = open-while-N + closed-while-Y
-            int penalty = prefixN[i] + suffixY[i];
-            if (penalty < minPenalty) { // Update min hour
+            // Update minimum penalty and hour
+            if (penalty < minPenalty) {
                 minPenalty = penalty;
-                minHour = i;
+                minHour = i + 1; // closing after hour i
             }
         }
         return minHour;
