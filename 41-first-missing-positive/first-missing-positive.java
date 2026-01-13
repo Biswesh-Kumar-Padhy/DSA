e03b0dcdb855f71be376be97e268437657abe05f
Biswesh-Kumar-Padhy <163874726+Biswesh-Kumar-Padhy@users.noreply.github.com>
2026-01-13 16:41:25 +0530

    Time: 2 ms (39.11%) | Memory: 71.6 MB (35.87%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,12 +1,28 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int num : nums){
-            set.add(num);
+        int n = nums.length;
+        boolean contains1 = false;
+
+         // Step 1: Check if 1 exists and replace invalid numbers with 1
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 1)   contains1 = true;               // track if 1 is present
+            if (nums[i] <= 0 || nums[i] > n)    nums[i] = 1;    // keep values in range [1, n]
         }
-        for(int i = 1; i < nums.length + 1; i++){
-            if(!set.contains(i))    return i;
+        if(!contains1)  return 1;   // If 1 is missing, it's the smallest positive
+
+        // Step 2: mark presence using index
+        for(int i = 0; i < n; i++){
+            int idx = Math.abs(nums[i]) - 1;   // convert value to index
+
+            if (nums[idx] > 0)
+                nums[idx] *= -1;    // negative means present
         }
-        return nums.length + 1;
+
+        // Step 3: First positive index means missing number
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0)
+                return i + 1;       // index + 1 is missing
+        }
+        return n + 1;               // If all 1..n are present, answer is n+1
     }
 }
